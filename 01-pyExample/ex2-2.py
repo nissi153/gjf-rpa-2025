727269670d02e8156d07af89cc5bd711140e945a
nissi153 <nissisoft21@gmail.com>
2025-07-07 16:45:48 +0900

    up

--- 01-pyExample/ex2-2.py
+++ 01-pyExample/ex2-2.py
@@ -41,3 +41,37 @@ print( "I eat %d apples" % 3 )
 print( "I eat %d apples, I sell %d apples" % (3, 2) )
 print( "%0.4f" % 3.42134234) # 소숫점 4자리까지 출력
 print( "%10.4f" % 3.42134234) # 전체자릿수 10자리, 소숫점 4자리까지 출력
+
+# 문자열 개수 세기
+a = "hobby"
+print( a.count('b') ) 
+
+# 문자 위치 찾기
+a = "Python is best choice"
+print("==="*5 + "문자 위치 찾기")
+print( a.find('b') ) # 10 인덱스에 있음
+print( a.find('k') ) # -1 못찾음
+print( a.index('n') )
+try:
+    print( a.index('k') ) # 못찾음 valueError
+except ValueError:
+    print("문자열을 찾을 수 없습니다.")    
+
+# 구분자 넣기
+a = ","
+print( a.join('abcd') ) # a,b,c,d
+
+# 양쪽 공백 지우기
+a = " HI "
+print( a.strip() ) 
+
+# 문자열 나누기
+a = "Life is too short"
+print( a.split() ) # 결과가 리스트로 반환됨. ['Life', 'is', 'too', 'short']
+a = "Life:is:too:short"
+print( a.split(':') ) # 구분자를 구체적으로 기술한다.
+
+# 문자열 바꾸기
+a = "Life is too short"
+print( a.replace('Life', "Your leg") )
+
