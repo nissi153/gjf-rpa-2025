f0b03fd1ef99e0eaeb3b50491ad3236901ae921b
nissi153 <nissisoft21@gmail.com>
2025-07-08 12:45:21 +0900

    update

---
+++ 01-pyExample/ex2-5.py
@@ -0,0 +1,20 @@
+# 파이썬의 주요 데이터 타입
+
+# 숫자 Number
+# 정수형 : int
+# 실수형 : float
+# 복소수형 : complex
+
+# 문자열 String
+
+# 리스트 List
+
+# 튜플 Tuple
+
+# 딕셔너리 Dictionary
+
+# 세트 Set
+
+# 불리언 Boolean
+
+# 값없음 NoneType
