fe8e437ac326aad3851465317dea66e1380317b4
nissi153 <nissisoft21@gmail.com>
2025-07-08 14:00:52 +0900

    Update ex2-6.py

--- 01-pyExample/ex2-6.py
+++ 01-pyExample/ex2-6.py
@@ -26,4 +26,25 @@ print( str(123) )
 print( str(3.14) )
 print( str([1,2,3]) )
 
+# 파이썬에서 False로 간주하는 값
+# 숫자 0 (정수,실수)
+# 빈 문자열 ""
+# 빈 리스트 []
+# 빈 튜플 ()
+# 빈 딕셔너리 {}
+# 빈 세트 set()
+# None
+
+# 위의 값 외에는 모두 True로 간주됩니다.
+print( bool(0) )
+print( bool(1) )
+print( bool(0.0) )
+print( bool("Hello") )
+print( bool("") )
+print( bool([]) )
+print( bool([1,2]) )
+print( bool(()) )
+print( bool(None) )
+
+
 
