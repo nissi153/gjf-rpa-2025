c475fd24362f29b895d0a65c53c46c896499ec53
nissi153 <nissisoft21@gmail.com>
2025-07-08 14:06:31 +0900

    Update ex2-6.py

--- 01-pyExample/ex2-6.py
+++ 01-pyExample/ex2-6.py
@@ -27,7 +27,7 @@ print( str(3.14) )
 print( str([1,2,3]) )
 
 # 파이썬에서 False로 간주하는 값
-# 숫자 0 (정수,실수)
+# 숫자 0 0.0 (정수,실수)
 # 빈 문자열 ""
 # 빈 리스트 []
 # 빈 튜플 ()
@@ -46,5 +46,12 @@ print( bool([1,2]) )
 print( bool(()) )
 print( bool(None) )
 
+print( list("Python") )
+print( list((1,2,3)) )
+print( list({1,2,3}) )
 
+print( tuple("Python") )
+print( tuple([1,2,3]) )
 
+print( set("Python") )
+print( set([1,2,3]) )
