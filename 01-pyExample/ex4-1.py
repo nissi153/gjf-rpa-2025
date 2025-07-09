7a3490d103e7fde22cd0174e038190446aaed49e
nissi153 <nissisoft21@gmail.com>
2025-07-09 16:16:20 +0900

    Update ex4-1.py

--- 01-pyExample/ex4-1.py
+++ 01-pyExample/ex4-1.py
@@ -21,6 +21,42 @@ my_list[1] = "hello"
 print( my_list )
 
 # my_list[3] = "d" # index out of range
-my_list.append("d")
+my_list.append("d") # 마지막에 추가
 print( my_list )
 
+my_list.insert(0, "z")
+print( my_list )
+
+other_list = ["x", "y"]
+my_list.extend( other_list )
+print( my_list )
+
+# 요소 삭제
+my_list = ['a','b','c','d','e']
+del my_list[2] # 인덱스 2의 'c' 삭제
+print( my_list ) # ['a', 'b', 'd', 'e']
+
+my_list.remove('b') # ['a', 'd', 'e']
+print( my_list )
+
+popped_item = my_list.pop(0) # 0번 인덱스를 삭제후 삭제된 리스트 반환한다.
+print(popped_item)
+print(my_list)
+
+my_list.clear() # 모든 요소 삭제
+print(my_list)
+
+# 리스트의 길이(요소갯수)
+print( len(mixed_list) )
+
+socores = [85, 92, 78, 92, 65]
+
+# 정렬( 오름차순 )
+socores.sort()
+print( socores ) # [65, 78, 85, 92, 92]
+
+socores.reverse()
+print( socores ) # [92, 92, 85, 78, 65]
+
+print( socores.count( 92 ) )
+print( socores.index( 78 ) )
