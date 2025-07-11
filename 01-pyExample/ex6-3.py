cd9bcd0e178eb2363202ccc2fbf5d2c96ad77d4d
nissi153 <nissisoft21@gmail.com>
2025-07-11 11:22:25 +0900

    update

---
+++ 01-pyExample/ex6-3.py
@@ -0,0 +1,23 @@
+# ex6-3.py
+# pandas 라이브러리로 CSV 파일 읽고 쓰기
+# pandas : 데이터분석에 특화된 라이브러리 
+#        : Data Frame 행열 2차원 데이터 처리에 매우 유용함.
+
+# pandas 라이브러리 설치
+# pip install pandas
+
+import pandas as pd
+
+# 데이터 프레임 생성(딕셔너리)
+data = {
+    "이름": ["홍길동", "김철수", "이영희"],
+    "나이": [30, 25, 35],
+    "도시": ["서울", "부산", "수원"]
+}
+# 데이터 프레임 객체 생성
+df = pd.DataFrame( data )
+print( df )
+
+# CSV 파일로 저장
+df.to_csv("people_pandas.csv", index=False, encoding='utf-8')
+print( "CSV 파일 저장 완료 " )
