e9f3d430ba180dcb42faecdc9a85fe9d2215b166
nissi153 <nissisoft21@gmail.com>
2025-07-11 10:24:03 +0900

    update

--- 01-pyExample/ex6-1.py
+++ 01-pyExample/ex6-1.py
@@ -1,3 +1,18 @@
 # ex6-1
 # 파일 입출력
 
+# 파일 생성하기
+# w 쓰기, r 읽기전용, a 추가
+f = open("test1.txt", 'w', encoding='utf-8')
+# 줄바꿈 특수문자 \n
+f.write('this is test file.\n테스트파일 입니다.')
+f.close() # 파일닫기
+
+# 파일 읽어오기
+f = open("test1.txt", 'r', encoding='utf-8')
+while True:
+    line = f.readline()
+    if not line: 
+        break
+    print( line )
+f.close()
